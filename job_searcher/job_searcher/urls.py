5dd40effbc45facbf6ebaa85c44207d2d46b5e75
EDownie <53910027+ED1978@users.noreply.github.com>
2020-01-27 07:06:03 +0000

    can display all jobs on front end

--- job_searcher/job_searcher/urls.py
+++ job_searcher/job_searcher/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from jobsearch.models import load_data
+from django.urls import path, include
 
 urlpatterns = [
+    path('jobs/', include('jobsearch.urls')),
     path('admin/', admin.site.urls),
 ]
-
-load_data()
--- job_searcher/jobsearch/views.py
+++ job_searcher/jobsearch/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from jobsearch.models import *
+
+def index(request):
+
+    load_data()
+
+    jobs = Job.objects.all()
+
+    return render(request, "jobsearch/index.html", locals())
 
 # Create your views here.
